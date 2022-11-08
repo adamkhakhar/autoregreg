48e586ec180560cb0047aaf4d20d17d90968868e
ak <akhakhar@allegro.seas.upenn.edu>
2022-11-08 10:48:58 -0500

    updating launch scripts for mae one dim and mnist

--- experiments/launch_scripts/mnist_mae_launch_script.sh
+++ experiments/launch_scripts/mnist_mae_launch_script.sh
@@ -1,12 +1,27 @@
-GPU_IND=0
+GPU_IND=7
 NUM_SAMPLES=10000000
 BATCH_SIZE=1000
-NUM_WORKERS=4
+NUM_WORKERS=2
 LOG_EVERY=100
-learning_rate=.01
 
 mkdir -p ../../results/result_logging/
-echo learning_rate $learning_rate
-name=mnist_mae_log_s_sin_l
-echo RUNNING $name
-python3 ../mnist_mae.py ${name} --num_samples $NUM_SAMPLES --batch_size $BATCH_SIZE --num_workers $NUM_WORKERS --gpu_ind $GPU_IND --log_every $LOG_EVERY --learning_rate $learning_rate --seed 1 >> ../../results/result_logging/${name}.log &
+for iteration in {1..10..1}
+do
+    echo iteration $iteration
+    learning_rate=.0001
+    name=mnist_mae_sin_s_log_s
+    echo RUNNING $name
+    python3 ../mnist_experiments/mnist_mae_sin_s_log_s.py ${name} --num_samples $NUM_SAMPLES --batch_size $BATCH_SIZE --num_workers $NUM_WORKERS --gpu_ind $GPU_IND --log_every $LOG_EVERY --learning_rate $learning_rate --seed $iteration >> ../../results/result_logging/${name}.log &
+
+    learning_rate=.005
+
+    name=mnist_mae_sin_s_log_l
+    echo RUNNING $name
+    python3 ../mnist_experiments/mnist_mae_sin_s_log_l.py ${name} --num_samples $NUM_SAMPLES --batch_size $BATCH_SIZE --num_workers $NUM_WORKERS --gpu_ind $GPU_IND --log_every $LOG_EVERY --learning_rate $learning_rate --seed $iteration  >> ../../results/result_logging/${name}.log &
+
+    name=mnist_mae_log_s_sin_l
+    echo RUNNING $name
+    python3 ../mnist_experiments/mnist_mae_log_s_sin_l.py ${name} --num_samples $NUM_SAMPLES --batch_size $BATCH_SIZE --num_workers $NUM_WORKERS --gpu_ind $GPU_IND --log_every $LOG_EVERY --learning_rate $learning_rate --seed $iteration  >> ../../results/result_logging/${name}.log &
+
+    wait
+done
