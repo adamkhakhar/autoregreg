5276dff6c4e3dc6e45d7a3440557fe7fca16bb90
ak <akhakhar@allegro.seas.upenn.edu>
2022-11-08 23:37:59 -0500

    launch script bug fix

--- experiments/launch_scripts/mnist_mae_lr_sensivity_launch_script.sh
+++ experiments/launch_scripts/mnist_mae_lr_sensivity_launch_script.sh
@@ -1,7 +1,7 @@
 GPU_IND=7
 NUM_SAMPLES=10000000
 BATCH_SIZE=1000
-NUM_WORKERS=2
+NUM_WORKERS=3
 LOG_EVERY=100
 
 mkdir -p ../../results/result_logging/
@@ -13,7 +13,7 @@ do
         echo target_scale $target_scale
         name=mnist_mae_lr_sensitivity
         echo RUNNING $name
-        python3 ../mnist_experiments/mnist_mae_lr_sensitivity.py ${name} --scale_targets $target_scale --num_samples $NUM_SAMPLES --batch_size $BATCH_SIZE --num_workers $NUM_WORKERS --gpu_ind $GPU_IND --log_every $LOG_EVERY --learning_rate $learning_rate --seed $iteration >> ../../results/result_logging/${name}.log &
+        python3 ../mnist_experiments/mnist_mae_lr_sensitivity.py ${name} --scale_targets $target_scale --num_samples $NUM_SAMPLES --batch_size $BATCH_SIZE --num_workers $NUM_WORKERS --gpu_ind $GPU_IND --log_every $LOG_EVERY --learning_rate $learning_rate --seed 1 >> ../../results/result_logging/${name}.log &
     done
     wait
 done
