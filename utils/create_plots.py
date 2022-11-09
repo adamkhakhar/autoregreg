8008194d8b6f1c5aa27981e61c4ffa65ec0ca374
ak <akhakhar@allegro.seas.upenn.edu>
2022-11-08 23:37:44 -0500

    additional functionality for plot creation

--- utils/create_plots.py
+++ utils/create_plots.py
@@ -11,16 +11,23 @@ def single_y_plot(
     labels=None,
     colors=None,
     line_style=None,
+    markers=None,
+    linewidth=None,
     x_axis_label=None,
     y_axis_label=None,
     x_axis_exp=False,
     y_axis_exp=False,
     title=None,
     legend=False,
+    tick_size=12,
+    label_size=15,
+    num_yaxis_ticks=6,
+    save_every_xth_ytick=2,
 ):
     assert len(x) == len(y)
     _, ax = plt.subplots()
     ax.spines["top"].set_color((0.8, 0.8, 0.8))
+    ax.spines["right"].set_color((0.8, 0.8, 0.8))
     plt.xlabel(x_axis_label)
     plt.ylabel(y_axis_label)
     plt.title(title, fontsize=20)
@@ -33,6 +40,10 @@ def single_y_plot(
             kwargs["color"] = colors[i]
         if line_style is not None:
             kwargs["linestyle"] = line_style[i]
+        if markers is not None:
+            kwargs["marker"] = markers[i]
+        if linewidth is not None:
+            kwargs["linewidth"] = linewidth[i]
         plt.plot(x[i], y[i], **kwargs)
 
     if legend:
@@ -46,14 +57,25 @@ def single_y_plot(
     xlab = ax.xaxis.get_label()
     ylab = ax.yaxis.get_label()
     xlab.set_style("italic")
-    xlab.set_size(10)
+    xlab.set_size(label_size)
     ylab.set_style("italic")
-    ylab.set_size(10)
+    ylab.set_size(label_size)
 
     # tweak the title
     ttl = ax.title
     ttl.set_weight("bold")
 
+    # tweak ticks
+    plt.xticks(fontsize=tick_size)
+    plt.yticks(fontsize=tick_size)
+    ax.set_yticks(
+        [
+            ax.get_yticks()[i]
+            for i in range(len(ax.get_yticks()))
+            if i % save_every_xth_ytick == 0
+        ]
+    )
+
     plt.tight_layout()
     plt.grid(linestyle="--", alpha=0.25)
-    plt.savefig(f"{save_path}/{save_title}", bbox_inches="tight")
+    plt.savefig(f"{save_path}/{save_title}.png", bbox_inches="tight")
